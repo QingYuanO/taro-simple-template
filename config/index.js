2b4dd33509823241cab5d27e896023d0cb0f835d
qingyuano <211720950@qq.com>
2022-09-01 20:28:49 +0800

    fix bug

--- config/index.js
+++ config/index.js
@@ -1,47 +1,47 @@
 //@ts-ignore
-import { resolve } from "path";
+import { resolve } from 'path';
 
 const {
   TaroWeappTailwindcssWebpackPluginV5,
-} = require("weapp-tailwindcss-webpack-plugin");
+} = require('weapp-tailwindcss-webpack-plugin');
 
-const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
 const config = {
   compiler: 'webpack5',
-  projectName: "wine-cellar-wechat",
-  date: "2021-11-19",
+  projectName: 'taro-simple-template',
+  date: '2021-11-19',
   designWidth: 750,
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
     828: 1.81 / 2,
   },
-  sourceRoot: "src",
+  sourceRoot: 'src',
   outputRoot: `dist/${process.env.TARO_ENV}`,
   plugins: [
     //本地插件
-    resolve(__dirname, "..", "src/plugin/index")
-      .split(":")[1]
-      .replace(/\\/g, "/"),
+    resolve(__dirname, '..', 'src/plugin/index')
+      .split(':')[1]
+      .replace(/\\/g, '/'),
   ],
   defineConstants: {},
   alias: {
-    "@/components": resolve(__dirname, "..", "src/components"),
-    "@/utils": resolve(__dirname, "..", "src/utils"),
-    "@/hooks": resolve(__dirname, "..", "src/hooks"),
-    "@/store": resolve(__dirname, "..", "src/store"),
-    "@/constants": resolve(__dirname, "..", "src/constants"),
-    "@/assets": resolve(__dirname, "..", "src/assets"),
-    "@/service": resolve(__dirname, "..", "src/service"),
-    "@/pages": resolve(__dirname, "..", "src/pages"),
-    "@/types": resolve(__dirname, "..", "src/types"),
+    '@/components': resolve(__dirname, '..', 'src/components'),
+    '@/utils': resolve(__dirname, '..', 'src/utils'),
+    '@/hooks': resolve(__dirname, '..', 'src/hooks'),
+    '@/store': resolve(__dirname, '..', 'src/store'),
+    '@/constants': resolve(__dirname, '..', 'src/constants'),
+    '@/assets': resolve(__dirname, '..', 'src/assets'),
+    '@/service': resolve(__dirname, '..', 'src/service'),
+    '@/pages': resolve(__dirname, '..', 'src/pages'),
+    '@/types': resolve(__dirname, '..', 'src/types'),
   },
   copy: {
     patterns: [],
     options: {},
   },
-  framework: "react",
+  framework: 'react',
   mini: {
     webpackChain(chain) {
       chain.merge({
@@ -55,7 +55,7 @@ const config = {
             args: [
               {
                 // 注意这一行(不传默认 react)
-                framework: "react", // 'vue2' / 'vue3'
+                framework: 'react', // 'vue2' / 'vue3'
               },
             ],
           },
@@ -76,15 +76,35 @@ const config = {
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: "module", // 转换模式，取值为 global/module
-          generateScopedName: "[name]__[local]___[hash:base64:5]",
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
         },
       },
     },
   },
   h5: {
-    publicPath: "/",
-    staticDirectory: "static",
+    webpackChain(chain) {
+      chain.merge({
+        plugins: [new LodashModuleReplacementPlugin()],
+      });
+
+      chain.merge({
+        plugin: {
+          install: {
+            plugin: TaroWeappTailwindcssWebpackPluginV5,
+            args: [
+              {
+                // 注意这一行(不传默认 react)
+                framework: 'react', // 'vue2' / 'vue3'
+                disabled: true,
+              },
+            ],
+          },
+        },
+      });
+    },
+    publicPath: '/',
+    staticDirectory: 'static',
     postcss: {
       autoprefixer: {
         enable: true,
@@ -93,8 +113,8 @@ const config = {
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: "module", // 转换模式，取值为 global/module
-          generateScopedName: "[name]__[local]___[hash:base64:5]",
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
         },
       },
     },
@@ -102,8 +122,8 @@ const config = {
 };
 
 export default function (merge) {
-  if (process.env.NODE_ENV === "development") {
-    return merge({}, config, require("./dev"));
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'));
   }
-  return merge({}, config, require("./prod"));
+  return merge({}, config, require('./prod'));
 }
