9ec390919f15fd326fba31e4476913621c732dc0
qingyuano <211720950@qq.com>
2022-12-14 21:58:50 +0800

    fix : 添加CountState type

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -2,28 +2,22 @@ import { Button } from '@antmjs/vantui';
 import { View, Text } from '@tarojs/components';
 import { memo } from 'react';
 import shallow from 'zustand/shallow';
-
 import create from 'zustand';
 
-const log = config => (set, get, api) =>
-  config(
-    (...args) => {
-      console.log('  applying', args);
-      set(...args);
-      console.log('  new state', get());
-    },
-    get,
-    api
-  );
+interface CountState {
+  count: number;
+  plus: () => void;
+  minus: () => void;
+}
 
-export const useBeeStore = create(set => ({
+export const useCountStore = create<CountState>()((set, get, api) => ({
   count: 0,
   plus: () => set(state => ({ count: state.count + 1 })),
   minus: () => set(state => ({ count: state.count - 1 })),
 }));
 
 const Count = () => {
-  const { count, plus, minus } = useBeeStore(state => ({ count: state.count, plus: state.plus, minus: state.minus }), shallow);
+  const { count, plus, minus } = useCountStore(state => state, shallow);
 
   return (
     <View className="mt-[30px] flex items-center">
