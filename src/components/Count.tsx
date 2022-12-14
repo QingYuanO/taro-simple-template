c4579ae64260eea8a3567f79c55ef36ddc21a7cf
qingyuano <211720950@qq.com>
2022-12-14 21:44:57 +0800

    feat : 修改状态管理为zustand

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,17 +1,35 @@
 import { Button } from '@antmjs/vantui';
 import { View, Text } from '@tarojs/components';
-import { atom, useAtom } from 'jotai';
 import { memo } from 'react';
+import shallow from 'zustand/shallow';
 
-const countAtom = atom(0);
+import create from 'zustand';
+
+const log = config => (set, get, api) =>
+  config(
+    (...args) => {
+      console.log('  applying', args);
+      set(...args);
+      console.log('  new state', get());
+    },
+    get,
+    api
+  );
+
+export const useBeeStore = create(set => ({
+  count: 0,
+  plus: () => set(state => ({ count: state.count + 1 })),
+  minus: () => set(state => ({ count: state.count - 1 })),
+}));
 
 const Count = () => {
-  const [count, setCount] = useAtom(countAtom);
+  const { count, plus, minus } = useBeeStore(state => ({ count: state.count, plus: state.plus, minus: state.minus }), shallow);
+
   return (
     <View className="mt-[30px] flex items-center">
-      <Button round icon="minus" className="text-btn" onClick={() => setCount(count - 1)} />
+      <Button round icon="minus" className="text-btn" onClick={minus} />
       <Text className="mx-[30px] ">{count}</Text>
-      <Button round icon="plus" className="text-btn" onClick={() => setCount(count + 1)} />
+      <Button round icon="plus" className="text-btn" onClick={plus} />
     </View>
   );
 };
---
+++ src/pages/index/store.ts
@@ -0,0 +1,19 @@
+import create from "zustand"
+
+const log = (config) => (set, get, api) =>
+  config(
+    (...args) => {
+      console.log('  applying', args)
+      set(...args)
+      console.log('  new state', get())
+    },
+    get,
+    api
+  )
+
+export  const useBeeStore = create(
+  log((set) => ({
+    bees: false,
+    setBees: (input) => set({ bees: input }),
+  }))
+)
