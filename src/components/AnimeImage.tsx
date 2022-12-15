f0ba41b9d10623192e99934c3d4ada7ca7e10901
chenhu <chenhu@reedsec.com>
2022-12-15 16:33:42 +0800

    update : 修改react-query示例

--- src/components/AnimeImage.tsx
+++ src/components/AnimeImage.tsx
@@ -5,30 +5,19 @@ import { memo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 const AnimeImage = () => {
-  const { data, isLoading, isSuccess, refetch } = useQuery(
-    ['getSingleImg'],
-    getSingleImg
-  );
+  const { data, isFetching, refetch } = useQuery(['getSingleImg'], getSingleImg);
   console.log(data);
 
   const onGetImage = async () => {
     refetch();
   };
   return (
-    <View className='mt-[100px] flex flex-col'>
-      <View
-        className='flex min-h-[600px] w-[500px] flex-col items-center justify-center shadow-lg'
-        onClick={onGetImage}
-      >
+    <View className="mt-[100px] flex flex-col">
+      <View className="flex min-h-[600px] w-[500px] flex-col items-center justify-center shadow-lg" onClick={onGetImage}>
         {data?.url && (
           <>
-            <Image
-              src={data.url}
-              className='h-full w-full'
-              style={{ display: isSuccess ? 'inherit' : 'none' }}
-              mode='widthFix'
-            />
-            {isLoading && <Text>加载中...</Text>}
+            <Image src={data.url} className="h-full w-full" style={{ display: !isFetching ? 'inherit' : 'none' }} mode="widthFix" />
+            {isFetching && <Text>加载中...</Text>}
           </>
         )}
       </View>
