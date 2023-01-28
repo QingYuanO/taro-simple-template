7384388efe18a504b9e1f324068195d59101081b
chenhu <chenhu@reedsec.com>
2023-01-28 10:59:04 +0800

    update : 修改IconFont 为QIcon

--- src/components/QIcon/index.tsx
+++ src/components/QIcon/index.tsx
@@ -6,24 +6,19 @@ import { View } from '@tarojs/components';
 import { ViewProps } from '@tarojs/components/types/View';
 import IconFont, { IconNames } from '../OriginalIconFont';
 
-
-
 interface Props {
   name: IconNames;
   size?: number;
   color?: string | string[];
 }
 
-const ReedIcon: FunctionComponent<Props & ViewProps> = (props) => {
-  const { name, size, color, style, className,...other } = props;
+const QIcon: FunctionComponent<Props & ViewProps> = props => {
+  const { name, size = 40, color, style, className, ...other } = props;
   return (
-    <View style={{ display: "inline-flex", ...(style as CSSProperties) }} {...other} className={className}>
+    <View style={{ display: 'inline-flex', ...(style as CSSProperties) }} {...other} className={className}>
       <IconFont name={name} size={size} color={color} />
     </View>
   );
 };
-ReedIcon.defaultProps = {
-  size: 18,
-};
 
-export default ReedIcon;
+export default QIcon;
