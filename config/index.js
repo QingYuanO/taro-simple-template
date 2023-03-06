4b056116d4cf8468265875c910dbb9c6eeb8f3dd
chenhu <chenhu@reedsec.com>
2023-03-06 18:08:29 +0800

    fix : 修复react-query-kit在h5环境下无法使用的bug

--- config/index.js
+++ config/index.js
@@ -47,7 +47,11 @@ const config = {
   },
   framework: 'react',
   compiler: {
-    type: 'webpack5',
+    type: "webpack5",
+    // 仅 webpack5 支持依赖预编译配置
+    prebundle: {
+      enable: false,
+    },
   },
   cache: {
     enable: false, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
