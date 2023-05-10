4fc1cfdb70b44884e1b7e148f2b615b3f0c35e26
chenhu <chenhu@reedsec.com>
2023-05-10 14:27:22 +0800

    feat : 添加有过期时间的TaroStorage

--- src/utils/TaroStorage.ts
+++ src/utils/TaroStorage.ts
@@ -1,6 +1,8 @@
 import Taro from '@tarojs/taro';
 import { StateStorage } from 'zustand/middleware';
 
+import storage from './storage';
+
 const TaroStorage: StateStorage = {
   getItem(name) {
     return Taro.getStorageSync(name);
@@ -11,6 +13,25 @@ const TaroStorage: StateStorage = {
   removeItem(name) {
     Taro.removeStorageSync(name);
   },
+};
+
+/**
+ * 获取可以过期的 TaroStorage
+ * @param exp 过期时间，单位毫秒
+ * @returns
+ */
+export const getExpTaroStorage = (exp: number): StateStorage => {
+  return {
+    getItem(name) {
+      return storage.getLocalStorage(name);
+    },
+    setItem(name, value) {
+      storage.setLocalStorage(name, value, exp);
+    },
+    removeItem(name) {
+      storage.removeLocalStorage(name);
+    },
+  };
 };
 
 export default TaroStorage;
