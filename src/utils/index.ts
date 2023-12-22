90087b97ffb6668bee34bde2bfc14f85495fb70b
qingyuano <211720950@qq.com>
2023-12-22 21:42:26 +0800

    feat: 更新依赖

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+// import { createTwc } from 'react-twc';
 import Taro, { EventChannel } from '@tarojs/taro';
 import { cx } from 'class-variance-authority';
 import { ClassValue } from 'class-variance-authority/dist/types';
@@ -17,3 +18,5 @@ export const pt = (num: number) => {
 export function cn(...inputs: ClassValue[]) {
   return twMerge(cx(inputs));
 }
+
+// export const twx = createTwc({ compose: cn });
