7f024304690084730534ae222bd1a7dc2aff06ff
qingyuano <211720950@qq.com>
2023-12-22 21:44:44 +0800

    chore: 更新依赖

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 // import { createTwc } from 'react-twc';
 import Taro, { EventChannel } from '@tarojs/taro';
-import { cx } from 'class-variance-authority';
 import { ClassValue } from 'class-variance-authority/dist/types';
+import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 //只能在微信小程序中使用
@@ -16,7 +16,7 @@ export const pt = (num: number) => {
 };
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(cx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 // export const twx = createTwc({ compose: cn });
