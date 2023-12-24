fee876ddb1452fa67df40be409f097130a95463b
qingyuano <211720950@qq.com>
2023-12-24 21:28:43 +0800

    fix: eslint 报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: ['taro/react', 'prettier'],
   plugins: ['prettier'],
+  parser: ['@typescript-eslint/parser'],
   rules: {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
